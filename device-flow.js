0a7a1923bd8d5ec7311c3824e41b65a166386194
Pushp Abrol <pushpabrol@nd4l9r5x9k.lan>
2022-08-11 00:21:26 -0400

    updates

--- device-flow.js
+++ device-flow.js
@@ -2,11 +2,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 import  open from "open";
 import { Issuer } from 'openid-client';
+import QRCode from 'qrcode';
+
 
 
 import readline from "readline";
 
-const issuer = await Issuer.discover(`https://${process.env.DOMAIN}`);
+const auth0Issuer = await Issuer.discover(`https://${process.env.DOMAIN}`);
 //console.log('Discovered issuer %s %O', auth0Issuer.issuer, auth0Issuer.metadata);
 
 const GRANT_TYPE = 'urn:ietf:params:oauth:grant-type:device_code';
@@ -24,10 +26,24 @@ const handle = await client.deviceAuthorization();
 console.log(handle);
 
 
-await open(handle.verification_uri_complete, { wait: false });
+//await open(handle.verification_uri_complete, { wait: false });
+var i = handle.expires_in;
+
+var timer = waitingForVerification(i); 
+
+QRCode.toString(handle.verification_uri_complete,{type:'terminal',margin:4, scale: 1, small: true},
+                    function (err, QRcode) {
+    if(err) return console.log("error occurred")
+    // Printing the generated code
+    console.log(QRcode);
+  
+
+})
 
 
 const tokenSet = await handle.poll()
+console.log("\n Done!");
+clearInterval(timer);
 
   console.log('got', tokenSet);
   console.log('id token claims', tokenSet.claims());
@@ -39,4 +55,21 @@ const tokenSet = await handle.poll()
     console.error(err);
     process.exitCode = 1;
   });
+
+  
+  function waitingForVerification(i) {
+
+    var countdownTimer = setInterval(function() {
+      process.stdout.clearLine();
+      process.stdout.cursorTo(0);
+        process.stdout.write("Expires in " + i + " seconds!");
+        i = i - 1;
+
+        if (i <= 0) {
+            clearInterval(countdownTimer);
+        }
+
+    }, 1000);
+return countdownTimer;
+  }
 
